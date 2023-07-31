a46fee0e791a0bc89310fd2d11286b516dc916ec
unknown <dawitkebedewoldu@gmail.com>
2023-07-31 05:04:29 -0700

    forth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider,Box,Heading,Button } from "@chakra-ui/react"
 import  "./style.css"
+import _ from "lodash"
+
 function App() {
   
-
+  console.log(_.chunk(["a","b","c","d"],2))
   return (
   <Box id="container">
-    <Box id="upper"></Box>
+    <Box id="upper">
+    </Box>
     <Box id="middle"></Box>
     <Box id="lower"></Box>
   </Box>
